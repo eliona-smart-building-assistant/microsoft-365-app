f66e7425ca88abcd403d91d0ffedabb60c1319bd
Zdenek Devaty <zdenek.devaty@eliona.io>
2023-06-09 11:36:04 +0200

    Add API definition

--- apiserver/model_configuration.go
+++ apiserver/model_configuration.go
@@ -9,18 +9,51 @@
 
 package apiserver
 
-// Configuration - Simply a example configuration
+// Configuration - Each configuration defines access to MS Graph.
 type Configuration struct {
 
-	// A id identifying the example configuration
+	// Internal identifier for the configured API (created automatically).
 	Id *int64 `json:"id,omitempty"`
 
-	// Configuration data for example
-	Config string `json:"config,omitempty"`
+	// Client ID
+	ClientId string `json:"clientId,omitempty"`
+
+	// Client Secret
+	ClientSecret string `json:"clientSecret,omitempty"`
+
+	// Tenant ID
+	TenantId string `json:"tenantId,omitempty"`
+
+	// Username
+	Username *string `json:"username,omitempty"`
+
+	// Password
+	Password *string `json:"password,omitempty"`
+
+	// Flag to enable or disable fetching from this API
+	Enable *bool `json:"enable,omitempty"`
+
+	// Interval in seconds for collecting data from API
+	RefreshInterval int32 `json:"refreshInterval,omitempty"`
+
+	// Timeout in seconds
+	RequestTimeout *int32 `json:"requestTimeout,omitempty"`
+
+	// Array of rules combined by logical OR
+	AssetFilter [][]FilterRule `json:"assetFilter,omitempty"`
+
+	// Set to `true` by the app when running and to `false` when app is stopped
+	Active *bool `json:"active,omitempty"`
+
+	// List of Eliona project ids for which this device should collect data. For each project id all smart devices are automatically created as an asset in Eliona. The mapping to Eliona assets is stored as an asset mapping in the MS Graph app.
+	ProjectIDs *[]string `json:"projectIDs,omitempty"`
 }
 
 // AssertConfigurationRequired checks if the required fields are not zero-ed
 func AssertConfigurationRequired(obj Configuration) error {
+	if err := AssertRecurseFilterRuleRequired(obj.AssetFilter); err != nil {
+		return err
+	}
 	return nil
 }
 
