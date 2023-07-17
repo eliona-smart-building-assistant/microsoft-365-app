638f3ac32895bdb999d9d8c83e38a94dc0433916
Zdenek Devaty <zdenek.devaty@eliona.io>
2023-07-17 08:44:43 +0200

    Add equipment asset type

--- eliona/init.go
+++ eliona/init.go
@@ -32,5 +32,8 @@ func InitEliona(connection db.Connection) error {
 	if err := asset.InitAssetTypeFile("eliona/asset-type-room-list.json")(connection); err != nil {
 		return fmt.Errorf("init room list asset type: %v", err)
 	}
+	if err := asset.InitAssetTypeFile("eliona/asset-type-equipment.json")(connection); err != nil {
+		return fmt.Errorf("init equipment asset type: %v", err)
+	}
 	return nil
 }
